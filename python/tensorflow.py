737bec4ebac6a191a922d27273d622db78f14c3e
Abhishek Parab <abhishekparab@gmail.com>
2019-10-11 11:01:37 -0500

    tensorflow: using a part of the GPU.

--- python/tensorflow.py
+++ python/tensorflow.py
@@ -4,3 +4,6 @@
 import os
 os.environ["CUDA_VISIBLE_DEVICES"]="0"
 os.environ["CUDA_VISIBLE_DEVICES"]="0,1"
+
+# Using a fraction of the GPU:
+gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.333)
