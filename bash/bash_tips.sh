0bc58feead038a1f198d993a156c82c38f80a2ea
Abhishek Parab <36110694+aaparab@users.noreply.github.com>
2019-11-21 09:54:07 -0600

    Added general bash tips file.

---
+++ bash/bash_tips.sh
@@ -0,0 +1,9 @@
+##############################
+#  General bash tips
+##############################
+
+# See (sorted) size of all files 
+# Reference: https://askubuntu.com/a/363681
+
+du -sch .[!.]* * |sort -h
+
