d0b6168a9be5bdcb5b16b461c248579c377aaf07
Abhishek Parab <36110694+aaparab@users.noreply.github.com>
2019-12-17 09:13:57 -0600

    Added watch command.
    Performs a linux command repeatedly.

--- bash/bash_tips.sh
+++ bash/bash_tips.sh
@@ -7,3 +7,8 @@
 
 du -sch .[!.]* * |sort -h
 
+# Perform a command repeatedly
+# Reference: https://www.tecmint.com/run-repeat-linux-command-every-x-seconds/
+
+watch --difference --interval 5 nvidia-smi
+watch script.sh
