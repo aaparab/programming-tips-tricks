d4c5ed18eff304ca132bef4a1c122a6c8117b8c0
Abhishek Parab <36110694+aaparab@users.noreply.github.com>
2020-01-09 10:55:37 -0600

    Change timezone on linux machine without admin access

--- bash/bash_tips.sh
+++ bash/bash_tips.sh
@@ -7,8 +7,21 @@
 
 du -sch .[!.]* * |sort -h
 
+
+
 # Perform a command repeatedly
 # Reference: https://www.tecmint.com/run-repeat-linux-command-every-x-seconds/
 
 watch --difference --interval 5 nvidia-smi
 watch script.sh
+
+
+
+# Change system timezone without SUDO access
+# (for instance, remote server, cloud).
+# Reference: https://superuser.com/a/856673
+
+# vi .bashrc
+export TZ="/usr/share/zoneinfo/{TIMEZONE-DIRECTORY}/{TIMEZONE_FILE}"
+
+
