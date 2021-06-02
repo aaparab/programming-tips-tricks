eecdd9df3d0be726f5dd84b7d64c8b86a1768982
Abhishek Parab <36110694+aaparab@users.noreply.github.com>
2021-06-02 07:37:02 -0500

    jupyter lab configurations

---
+++ config/jupyter-config.py
@@ -0,0 +1,73 @@
+##########
+# Notebook
+##########
+{
+    "codeCellConfig": {
+        "fontSize": 15, 
+        "fontFamily": "Monaco"
+    }
+}
+
+
+
+
+
+##########
+# Terminal
+##########
+{
+    // Terminal
+    // @jupyterlab/terminal-extension:plugin
+    // Terminal settings.
+    // *************************************
+
+    // Font size
+    // The font size used to render text.
+    "fontSize": 14, 
+    "fontFamily": "Monaco"
+}
+
+
+
+
+##########
+# Text Editor
+##########
+"editorConfig": {
+        "fontFamily": "Monaco",
+        "fontSize": 14,
+        "lineHeight": null,
+        "lineNumbers": true,
+        "lineWrap": "on",
+        "wordWrapColumn": 80,
+        "readOnly": false,
+        "tabSize": 4,
+        "insertSpaces": true,
+        "matchBrackets": true,
+        "autoClosingBrackets": true,
+        "rulers": [],
+        "codeFolding": false
+    }
+}
+
+
+
+
+
+##########
+# Theme
+##########
+{
+    // Theme
+    // @jupyterlab/apputils-extension:themes
+    // Theme manager settings.
+    // *************************************
+
+    // Selected Theme
+    // Application-level visual styling theme
+    "theme": "JupyterLab Dark",
+
+    // Scrollbar Theming
+    // Enable/disable styling of the application scrollbars
+    "theme-scrollbars": true
+}
